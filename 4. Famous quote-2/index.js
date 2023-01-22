4903a4d3e3f7652c88f342df1a0e270889b73e82
mehakgmd <115011143+mehakgmd@users.noreply.github.com>
2023-01-22 18:41:48 +0500

    Add files via upload

---
+++ 4. Famous quote-2/index.js
@@ -0,0 +1,6 @@
+const Quotes = {
+    name: "Albert Einstien",
+    quote: "person who never made a mistake never tried anything new"
+};
+
+console.log(`${Quotes.name} once said:   "${Quotes.quote}"`);
